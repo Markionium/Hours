ef2c55fdc8f4d9e356eec21a10c73f810f8461fe
Mark Polak <mark@thedutchies.com>
2014-06-17 23:12:00 +0200

    Add simple hour specs

--- src/main/js/hours.js
+++ src/main/js/hours.js
@@ -2,10 +2,29 @@
 
 var Hours = function () {
     var hours = {},
-        logger = Logger();
+        logger = Logger(),
+        tasks = {};
 
     hours.log = logger.log;
     hours.getHours = logger.getHours;
+
+    hours.addTask = function (name) {
+        tasks[name] = { "name": name };
+    }
+
+    hours.getTask = function (name) {
+        return tasks[name];
+    }
+
+    hours.getTasks = function () {
+        var key,
+            result = [];
+
+        for (key in tasks) {
+            result.push(tasks[key]);
+        }
+        return result;
+    }
 
     function Hours () {};
     Hours.prototype = hours;
--- src/specs/hours_spec.js
+++ src/specs/hours_spec.js
@@ -1,3 +1,22 @@
-/**
- * Created by mark on 15/06/14.
- */
+'use strict';
+
+describe('Hours', function () {
+
+    var hours;
+
+    beforeEach(function () {
+        hours = Hours();
+    });
+
+    it('Should add a task to the list', function () {
+        hours.addTask('MyTask');
+
+        expect(hours.getTasks()).toEqual([ {"name": "MyTask"} ]);
+    });
+
+    it('Should get a task by name and return object with that task', function () {
+        hours.addTask('MyTask');
+
+        expect(hours.getTask('MyTask')).toEqual( {"name": "MyTask"} );
+    });
+});
